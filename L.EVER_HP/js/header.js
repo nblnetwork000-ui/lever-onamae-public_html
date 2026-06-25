98ca169d8d954c565138e1cebd9674b070c219f1
nblnetwork000-ui <nblnetwork.000@gmail.com>
2026-06-25 11:14:57 +0900

    Add files via upload

---
+++ L.EVER_HP/js/header.js
@@ -0,0 +1,8 @@
+const hamburger =
+document.getElementById("hamburger");
+
+const mobileMenu =
+document.getElementById("mobileMenu");
+hamburger.addEventListener("click", function(){
+    mobileMenu.classList.toggle("active");
+});
